19110028d12550e1aa0d306c502c38e41fa1ce38
paulo <olivigeoarte@gmail.com>
2013-11-19 11:50:50 -0200

    fhfh

--- mapa.js
+++ mapa.js
@@ -20,7 +20,7 @@ function showPosition(position)
 var latlon=position.coords.latitude+","+position.coords.longitude;
 
 var img_url="http://maps.googleapis.com/maps/api/staticmap?center="
-+latlon+"&zoom=14&size=400x300&sensor=false";
++latlon+"&zoom=14&size=300x300&sensor=false";
 
 document.getElementById("mapholder").innerHTML="<img src='"+img_url+"'>";
 }
